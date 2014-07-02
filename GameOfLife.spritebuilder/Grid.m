d706d0a8d3d19530e6b6df49cbf8a713b8c31453
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-02 21:53:49 +1000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -1,6 +1,8 @@
 
 #import "Grid.h"
 #import "Creature.h"
+//#import "Creature.m"
+
 
 // these are variables that cannot be changed
 static const int GRID_ROWS = 8;
@@ -136,9 +138,57 @@ static const int GRID_COLUMNS = 10;
 }
 - (void)updateCreatures
 {
+    // iterate through the rows
+    // note that NSArray has a method 'count' that will return the number of elements in the array
+    for (int i = 0; i < [_gridArray count]; i++)
+    {
+        // iterate through all the columns for a given row
+        for (int j = 0; j < [_gridArray[i] count]; j++)
+        {
+            // access the creature in the cell that corresponds to the current row/column
+            Creature *currentCreature = _gridArray[i][j];
+            
+            // remember that every creature has a 'livingNeighbors' property that we created earlier
+            currentCreature.livingNeighbors = 0;
+            
+            // now examine every cell around the current one
+            
+            // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
+            for (int x = (i-1); x <= (i+1); x++)
+            {
+                // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
+                for (int y = (j-1); y <= (j+1); y++)
+                {
+                    // check that the cell we're checking isn't off the screen
+                    BOOL isIndexValid;
+                    isIndexValid = [self isIndexValidForX:x andY:y];
+                    
+                    // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
+                    if (!((x == i) && (y == j)) && isIndexValid)
+                    {
+                        Creature *neighbor = _gridArray[x][y];
+                        if (neighbor.isAlive)
+                        {
+                            currentCreature.livingNeighbors += 1;
+                            if (currentCreature.livingNeighbors == 3) {
+                                currentCreature.isAlive = TRUE;
+                            }
+                            if (currentCreature.livingNeighbors <= 1){
+                                currentCreature.isAlive = FALSE;
+                            }
+                            if (currentCreature.livingNeighbors >= 4) {
+                                currentCreature.isAlive = FALSE;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
     
 }
-
+_totalAlive = numAlive;
 
 
 
