275febc6edcde775033e887fba7ea1999ef49f80
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-14 18:58:51 +1000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -169,26 +169,14 @@ static const int GRID_COLUMNS = 10;
 //_totalAlive = numAlive;
 //}
 -(void)updateCreatures
-{ //int numAlive = 0;
+
+{_totalAlive=0;
     for (int i = 0; i < [_gridArray count]; i++)
     {
      //iterate through all the columns for a given row
     for (int j = 0; j < [_gridArray[i] count]; j++)
     
         { Creature *currentCreature = _gridArray[i][j];
-        
-        
-//        // now examine every cell around the current one
-//        
-//        // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
-//        for (int x = (i-1); x <= (i+1); x++)
-//        {
-//            // go through the column to the left of the current cell, the column the cell is in, and the
-//            //column to the right of the current cell
-//            for (int y = (j-1); y <= (j+1); y++)
-//            
-            
-        
         
         
        if (currentCreature.livingNeighbors==3) { currentCreature.isAlive= TRUE; }
